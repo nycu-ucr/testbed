6a41c087957d0c3fdc0f2c0b919a45d75513a798
hstsai <jerry10127@gmail.com>
2022-12-08 02:27:49 +0800

    Add testing persistent connection

--- http/http_client/http_client.go
+++ http/http_client/http_client.go
@@ -50,14 +50,15 @@ func main() {
 	ID = 50
 
 	for i := 0; i < EPOCHS; i++ {
-		http2Post("http://127.0.0.2:8000/test-server/PostUser")
-		http2GET("http://127.0.0.2:8000/test-server/GetUser")
-		http2Post("http://127.0.0.2:8000/test-server/PostUser")
-		http2GET("http://127.0.0.2:8000/test-server/GetUser")
-		http2GET("http://127.0.0.2:8000/test-server/GetUser/1")
+		// http2Post("http://127.0.0.2:8000/test-server/PostUser")
+		// http2GET("http://127.0.0.2:8000/test-server/GetUser")
+		// http2Post("http://127.0.0.2:8000/test-server/PostUser")
+		// http2GET("http://127.0.0.2:8000/test-server/GetUser")
+		// http2GET("http://127.0.0.2:8000/test-server/GetUser/1")
+		http2PostMultiple("http://127.0.0.2:8000/test-server/PostUser")
 	}
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(20 * time.Second)
 }
 
 func httpGET(url string) {
@@ -213,4 +214,60 @@ func http2GET(url string) {
 	logger.Log.Infof("URL: %s\n\u001b[92m[Response]\u001b[0m \n%s\n\u001b[95m[Delay]\u001b[0m %f", url, rsp, delay)
 
 	return
+}
+
+func http2PostMultiple(url string) {
+	// var err error
+	var t http.RoundTripper
+
+	if !USE_ONVM_TRANSPORT {
+		if USE_ONVM {
+			logger.Log.Warnln("Using ONVM socket")
+			t = &http2.Transport{
+				AllowHTTP: true,
+				DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+					return onvmpoller.DialONVM("onvm", addr)
+				},
+			}
+		} else {
+			logger.Log.Warnln("Using TCP socket")
+			t = &http2.Transport{
+				AllowHTTP: true,
+				DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+					return net.Dial("tcp", addr)
+				},
+			}
+		}
+	} else {
+		logger.Log.Warnln("Using http2.OnvmTransport")
+		t = &http2.OnvmTransport{
+			UseONVM: true,
+		}
+	}
+
+	c := &http.Client{
+		Transport: t,
+	}
+
+	for i := 0; i < 5; i++ {
+		user := &User{
+			Id:       strconv.Itoa(ID),
+			Name:     fmt.Sprintf("Ben%d", ID),
+			Password: "qqqqq",
+		}
+		ID = ID + 1
+		b, err := json.Marshal(user)
+
+		t1 := time.Now()
+		resp, err := c.Post(url, "application/json", bytes.NewReader(b))
+		rsp, _ := ioutil.ReadAll(resp.Body)
+		t2 := time.Now()
+
+		delay := t2.Sub(t1).Seconds() * 1000
+		if err != nil {
+			logger.Log.Fatal("request error")
+		}
+		logger.Log.Infof("URL: %s\n\u001b[92m[Response]\u001b[0m \n%s\n\u001b[95m[Delay]\u001b[0m %f", url, rsp, delay)
+		resp.Body.Close()
+	}
 }
